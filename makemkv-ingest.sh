45a4e617659cfd6b0688f536287f5481e73016ac
Lily Hughes-Robinson <oaguy1@gmail.com>
2023-01-10 22:00:09 -0500

    First version of script

--- makemkv-ingest.sh
+++ makemkv-ingest.sh
@@ -1,3 +1,26 @@
 #! /usr/bin/env bash
 
-echo "FUCK YOU"
+BASE_DIR="$HOME/Videos"
+
+# Do Forever
+while true
+do
+	# sleep until a disk is inserted
+	until sudo blkid /dev/sr0 > /dev/null
+	do
+		sleep 10
+	done
+
+	# Find the disc label using blkid and save to var
+	DISC_LABEL=$(sudo blkid /dev/sr0 | awk '{print $3}' | awk -F "=" '{gsub(/"/, "", $2); print $2}')
+	echo "$DISC_LABEL"
+
+	# remove any existing backups of disc
+	rm -rf  "$BASE_DIR/$DISC_LABEL.iso"
+
+	# make disc backup, then eject disk
+	makemkvcon backup disc:/dev/sr0 "$BASE_DIR/$DISC_LABEL.iso"
+	eject
+	sleep 10
+done
+
